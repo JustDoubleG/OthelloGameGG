cf39255a907b426c50a187db1b514c797b123375
JustDoubleG <44785300+JustDoubleG@users.noreply.github.com>
2018-11-05 14:56:21 -0600

    Add files via upload

--- Board.java
+++ Board.java
@@ -12,6 +12,46 @@ public class Board {
 		{0, 0, 0, 0, 0, 0, 0, 0 },
 	};
 	
-	System.out.println(board[4][4]);
+	public static int[] possibleMoves(int[][] b, Player p1){
+		//get Player color 
+		int color = p1.getColor();
+		int indexXArray[];
+		int indexYArray[];
+		int counterN = 0;
+		boolean isOne = true;
+		 for(int i = 0; i < 8; i++) {
+			 for(int j = 0; j < 8; i++) {
+				 if( (b[i][j] == color ) && (b[i+1][j] != color) && (b[i+2][j] ==0)){ //Checks downwards
+					 indexXArray[counterN] = i+2;
+					 indexYArray[counterN] = j;
+					 counterN+=1;
+				 }
+				 if( (b[i][j] == color ) && (b[i-1][j] != color) && (b[i-2][j] ==0)){ //Checks upwards
+					 indexXArray[counterN] = i-2;
+					 indexYArray[counterN] = j;
+					 counterN+=1;
+				 }
+				 if( (b[i][j] == color ) && (b[i][j+1] != color) && (b[i][j+2] ==0)){ //Checks upwards
+					 indexXArray[counterN] = i;
+					 indexYArray[counterN] = j+2;
+					 counterN+=1;
+				 }
+				 if( (b[i][j] == color ) && (b[i][j-1] != color) && (b[i][j-2] ==0)){ //Checks upwards
+					 indexXArray[counterN] = i;
+					 indexYArray[counterN] = j-2;
+					 counterN+=1;
+				 }
+		 }
+			 
+	 }
+	for(int t = 0; t <= counterN; t++) {
+		return indexXArray[t];
+		return indexyArray[t];
 	}
+	
+	
+	System.out.println(board[4][4]);
+	 
+	}
+}
 }
