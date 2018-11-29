69160626b251dfd6fa2704eee8535874a3895979
slowpoke03 <44785250+slowpoke03@users.noreply.github.com>
2018-11-28 21:03:16 -0600

    Add files via upload

---
+++ ZAdmin.java
@@ -0,0 +1,16 @@
+package othello;
+
+public class ZAdmin extends ZPlayer{
+	private boolean isAdmin;
+	
+	ZAdmin(){
+		this.setUsername("admin");
+		this.setPassword("admin");
+		this.isAdmin = true;
+	}
+	
+	public boolean getIsAdmin(){
+		return this.isAdmin;
+	}
+
+}
